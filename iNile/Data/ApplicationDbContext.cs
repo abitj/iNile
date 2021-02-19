31afbcd0c205507907005b8b23e62c614c122153
abitj <https://github.com/abitj>
2021-02-19 10:03:58 -0600

    release 1.0 completed

---
+++ iNile/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using iNile.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iNile.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options)
+        {
+        }
+        public DbSet<Category> Category { set; get; }
+        public DbSet<ApplicationType> ApplicationType { set; get; } 
+    }
+}
